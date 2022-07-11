098bc6a870c512a9f230e86431b359f5389a4997
Lütfü ÖĞRETİCİ <56653899+mogretici@users.noreply.github.com>
2022-07-12 00:51:54 +0300

    nativeFetch

---
+++ nativefetch/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Users from './components/Users.js' 
+import  React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+
+        <Users />
+
+    </div>
+  );
+}
+
+export default App;
