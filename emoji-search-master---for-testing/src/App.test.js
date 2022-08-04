d5ab0b8f0c69966abd889bc8d007b6dbd0159ac6
Lütfü ÖĞRETİCİ <56653899+mogretici@users.noreply.github.com>
2022-08-04 19:05:43 +0300

    testforpatika

--- emoji-search-master---for-testing/src/App.test.js
+++ emoji-search-master---for-testing/src/App.test.js
@@ -1,8 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import react from "react";
+import {screen, render} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Header from "./Header.js";
+ 
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe("Test for patika.dev", () => {
+  test("Başlık kısmının başarılı bir şekilde render edilmesi ", () => {
+
+  });
+  test("Uygulama ilk açıldığında emoji listesinin başarılı bir şekilde render edilmesi ", () => {
+
+  });
+  test("Bir filtreleme işlemi yapıldığında, emoji listesinin bu filtreye uygun şekilde yeniden render edilmesi ", () => {
+
+  });
+  test("Liste üzerinden herhangi emojiye tıklandığında, ilgili emojinin kopyalanması ", () => {
+
+  });
 });
