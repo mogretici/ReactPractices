d9778ecac0dd81ef3882808d409fbe5e56a4fec3
Lütfü ÖĞRETİCİ <56653899+mogretici@users.noreply.github.com>
2022-08-04 16:46:55 +0300

    toDo test added

--- testing/src/components/ToDo/ToDo.test.js
+++ testing/src/components/ToDo/ToDo.test.js
@@ -1,3 +1,37 @@
 import {screen, render} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import ToDo from "./index";
+
+
+describe ("ToDo Test", () => {
+    let button, input;
+
+    test('varsayılan itemların listelenmesi', ()=>{
+        render(<ToDo />);
+        button = screen.getByText('Add');
+        input = screen.getByPlaceholderText('What do you want to do?');
+        const items= screen.getAllByText(/Item/i);
+        expect(items.length).toEqual(3);  
+    })
+
+    test ('input ve buton dökümanda bulunmalı',()=>{
+        render(<ToDo />);
+        button = screen.getByText('Add');
+        input = screen.getByPlaceholderText('What do you want to do?');
+        expect(input).toBeInTheDocument();
+        expect(button).toBeInTheDocument();
+    })
+    test ('inputa string girilip butona basılınca listeye eklenmesi',()=>{
+        render(<ToDo />);
+        button = screen.getByText('Add');
+        input = screen.getByPlaceholderText('What do you want to do?');
+        //inputu doldur
+        const name='Lütfü';
+        userEvent.type(input,name);
+        //butona tıkla
+        userEvent.click(button);
+        //assertion
+        expect(screen.getByText(name)).toBeInTheDocument();
+    })
+
+});
--- testing/src/components/ToDo/index.js
+++ testing/src/components/ToDo/index.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 
 function ToDo() {
+  const defaultValue = ["Item 1", "Item 2", "Item 3"];
+  
   const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(defaultValue);
 
   const addTodoList = (e) => {
     e.preventDefault();
     if(todo==="") return false;
     setTodoList([...todoList, todo]);
     setTodo("");
-    
   };
   return (
     <div>
       <form onSubmit={addTodoList}>
           <h1>ToDoList </h1>
-          <input onChange={(e) =>{setTodo(e.target.value)}} value={todo} />
+          <input onChange={(e) =>{setTodo(e.target.value)}} value={todo} placeholder="What do you want to do?" />
           <button>Add</button>
       </form>
       {todoList.map((item, index) => {
